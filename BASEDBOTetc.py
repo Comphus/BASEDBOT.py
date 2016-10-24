652c14b243207524b450a63720c19e628e318d60
Gabriel Lopez <gabiota3000@yahoo.com>
2016-10-24 02:25:43 -0400

    Update BASEDBOTetc.py

--- BASEDBOTetc.py
+++ BASEDBOTetc.py
@@ -2,9 +2,22 @@ import requests
 import json
 import random
 import re
+import discord
+import asyncio
+import codecs
 
 with open("malinfo.json") as j:
 	malinfo = json.load(j)
+with open("MainResponses.json") as j:
+	MainResponses = json.load(j)
+with open('bnsemotes.txt') as inputF:
+	bnsEmotes = inputF.read().splitlines()
+
+unicodeResponses = {'/lenny':'( ͡° ͜ʖ ͡°)',
+	'!gardenintool':'(  ′︵‵  )/',
+	"!donkmay":"🎊🚢💗 **DONMAYKAY**2⃣0⃣1⃣6⃣ 💗🚢🎊",
+	"!oceanman":"OCEAN MAN 🌊 😍 Take me by the hand ✋ lead me to the land that you understand 🙌 🌊 OCEAN MAN 🌊 😍 The voyage 🚲 to the corner of the 🌎 globe is a real trip 👌 🌊 OCEAN MAN 🌊 😍 The crust of a tan man 👳 imbibed by the sand 👍 Soaking up the 💦 thirst of the land 💯"
+}
 
 def defines(message):
 	if len(message.content.split()) == 1:
@@ -63,7 +76,7 @@ def checktwitch(message):
 	if len(message.content.split()) != 2:
 		return 'The format of !checktwitch is `!checktwitch (channelname).`'
 	tChan = message.content.split()[-1].lower()
-	r = requests.get('https://api.twitch.tv/kraken/streams/{}/?&client_id=(your client id)'.format(tChan))
+	r = requests.get('https://api.twitch.tv/kraken/streams/{}/?&client_id=4asyzu8i1l7ea1f61aebw3mgbuv04y2'.format(tChan))
 	if r.status_code == 200:
 		tData = r.json()
 		if tData['stream'] == None:
@@ -74,3 +87,69 @@ def checktwitch(message):
 		return 'This channel does not exist.'
 	elif r.status_code == 422:
 		return 'Channel ' + tChan + ' is a justin.tv channel and doesnt work on twitch or is banned!'
+class botetc:
+	def __init__(self, bot):
+		self.bot = bot
+
+	async def regions(self, message):
+		for i in MainResponses["regions"]:
+			for j in message.author.roles:
+				if i in j.name and i.lower() == message.content.lower().replace('!', ''):
+					await self.bot.remove_roles(message.author, discord.utils.get(message.server.roles, name = i))
+					await asyncio.sleep(1)
+					return
+			if i.lower() == message.content.lower().replace('!', ''):
+				await self.bot.add_roles(message.author, discord.utils.get(message.server.roles, name = i))
+				await asyncio.sleep(1)
+				return
+	async def zealemotes(self, message):
+		for i in message.content.lower().split():
+			if i.startswith('!') and i.replace('!','') in bnsEmotes:
+				await self.bot.send_file(message.channel, 'C:/DISCORD BOT/bns_emotes/'+i.replace('!','')+'.png')
+				return
+	async def deleteian(self, message):
+		with codecs.open('daddy.txt','r',"utf-8") as f:
+			for i in f:
+				await self.bot.send_message(message.channel, i)
+				await asyncio.sleep(2)
+
+	async def bbresponse(self, message):
+		if 'who are you' in message.content.lower():
+			await self.bot.send_message(message.channel, 'I am a bot that runs on a community made python API(more info on that in bot-and-api channel) and programmed by Comphus to have functions for the Dragon Nest NA Community Discord Server')
+			return
+		if any(word in message.content.lower() for word in MainResponses['qQuestion']):
+			await self.bot.send_message(message.channel, MainResponses['magicEight'][random.randint(0,19)]+', ' +  message.author.mention)
+			return 
+		elif 'hi' in message.content.lower() or 'hello' in message.content.lower():
+			await self.bot.send_message(message.channel, 'Hi! ' + message.author.mention)
+		elif 'bye' in message.content.lower():
+			await self.bot.send_message(message.channel, 'Bye-Bye! ' + message.author.mention)
+		elif 'i love you' in message.content.lower() or '<3' in message.content:
+			await self.bot.send_message(message.channel, 'I love you too <3 ' + message.author.mention)
+		elif 'thank' in message.content.lower():
+			await self.bot.send_message(message.channel, 'You\'re welcome! ' + message.author.mention)
+		elif 'fuck you' in message.content.lower() or 'fuck u' in message.content.lower() or '( ° ͜ʖ͡°)╭∩╮' in message.content:
+			await self.bot.send_message(message.channel, '( ° ͜ʖ͡°)╭∩╮ ' + message.author.mention)
+		else:
+			await self.bot.send_message(message.channel, 'what? ' + message.author.mention)
+
+	async def spookme(self, message):
+		skeleR = random.randint(0,39)
+		if skeleR <=30:
+			await self.bot.send_message(message.channel, message.author.mention + ' YOU\'VE BEEN SPOOKED!')
+			await self.bot.send_file(message.channel, 'skele'+str(skeleR)+'.jpg')
+		elif skeleR <=38:
+			await self.bot.send_message(message.channel, message.author.mention + ' YOU\'VE BEEN SUPER SPOOKED!')
+			await self.bot.send_file(message.channel, 'skele'+str(skeleR)+'.jpg')
+		else:
+			await self.bot.send_message(message.channel, 'YOU\'VE BEEN SPOOKED TO DEATH\nhttps://www.youtube.com/watch?v=O8XfV8aPAyQ')
+
+	async def lightproc(self, message):
+		await self.bot.send_message(message.channel, 'Buckle up!')
+		await self.bot.send_file(message.channel, 'Comphus.jpg')
+
+	async def mainprefixcommands(self, message):
+		if message.content.lower().startswith("!commands"):
+			await self.bot.send_message(message.author, MainResponses['all!commands'][message.content.lower().split()[0]])
+		else:
+			await self.bot.send_message(message.channel, MainResponses['all!commands'][message.content.lower().split()[0]])
