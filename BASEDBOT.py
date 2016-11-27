62825aa9f3c25091285e27d6af44578b694f8430
Gabriel Lopez <gabiota3000@yahoo.com>
2016-11-26 23:35:58 -0500

    Update BASEDBOT.py

--- BASEDBOT.py
+++ BASEDBOT.py
@@ -1,3 +1,4 @@
+from discord.ext import commands
 import discord
 import asyncio
 import json
@@ -5,247 +6,64 @@ import datetime
 import random
 import importlib
 import sys
+bot = commands.Bot(command_prefix='!',description="I am a bot created by Comphus mainly for the Dragon Nest Community Discord server, but I have many other functions!")
 
-import BASEDBOTgames
-import BASEDBOTbns
-import BASEDBOTow
-import BASEDBOTdn
-import BASEDBOTetc
-import BASEDBOTmusic
-import BASEDBOTdiscord
-client = discord.Client()
+startup_ext = [
+	"botmodules.BASEDBOTgames",
+	"botmodules.BASEDBOTbns",
+	"botmodules.BASEDBOTow",
+	"botmodules.BASEDBOTdn",
+	"botmodules.BASEDBOTetc",
+	"botmodules.BASEDBOTmusic",
+	"botmodules.BASEDBOTdiscord"
+]
 
 with open("C:/discordlogin.json") as j:
 	dLogin = json.load(j)
 
-@client.event
-async def on_member_join(member):
-	await BASEDBOTdiscord.newmem(client, member).newmember()
+def is_owner(ctx):
+	return ctx.message.author.id == '90886475373109248'
 
-@client.event
-async def on_member_update(before, after):
-	if before.server.id == '106293726271246336':
-		if len(before.roles) == len(after.roles):
-			for i in after.roles:
-				if i.name == 'Streamer':
-					try:
-						if after.game.type == 1:
-							strm = BASEDBOTdn.streamer(client, after).DNstream()
-							await strm
-					except:
-						pass
+@bot.event
+async def on_command_error(error, ctx):
+	if isinstance(error, commands.NoPrivateMessage):
+		await bot.send_message(ctx.message.author, 'This command cannot be used in private messages.')
+	elif isinstance(error, commands.DisabledCommand):
+		await bot.send_message(ctx.message.author, 'Sorry. This command is disabled and cannot be used.')
+	#else:
+	#	print(error)
 
-@client.event
+@bot.command()
+@commands.check(is_owner)
+async def reload(extension_name : str):
+	try:
+		bot.unload_extension(extension_name)
+		bot.load_extension(extension_name)
+		await bot.say('module {} reloaded'.format(extension_name))
+	except Exception as e:
+		await bot.say('reloading failed, error is:```\n{}```'.format(e))
+
+@bot.event
 async def on_message(message):
-	if client.user == message.author or message.channel.id == '168949939118800896' or message.author.id in '128044950024617984':
+	if bot.user == message.author or message.channel.id == '168949939118800896' or message.author.id in '128044950024617984':
 		return
 
-	#basedbot discord commands
-	bbd = BASEDBOTdiscord.bbDiscord(client, message)
-	if message.content.startswith('!debug') and message.author.id == '90886475373109248':
-		await bbd.debug()
-	elif message.content.startswith('!removeallmsgs') and message.author.id == '90886475373109248':
-		await bbd.removeallmsgs()
-	elif message.content.startswith('!vanish') and message.content.split()[2].isdigit() and len(message.mentions) > 0:
-		await bbd.vanish()
-	elif message.content.startswith('!totalmem'):
-		await client.send_message(message.channel, "**Total Members:** {}".format(message.channel.server.member_count))
-	elif message.content.startswith("!voiceid"):
-		await client.send_message(message.channel, message.author.voice_channel.id)
-	elif message.content.startswith("!stats"):
-		await bbd.dStats()
-	elif message.content.startswith('!id'):
-		await bbd.dID()
-	elif message.content.startswith('!myinfo'):
-		await bbd.myinfo()
-	elif message.content.startswith('!info') and len(message.content.split()) > 1:
-		await bbd.info()
-	elif message.content.startswith('!avatar'):
-		await bbd.avatar()
-	elif message.content.startswith('!serverpic'):
-		await client.send_message(message.channel, message.channel.server.icon_url)
-	elif message.content.startswith('!chid'):
-		await client.send_message(message.channel, message.channel.id)
-	elif message.content.startswith('!serverid'):
-		await client.send_message(message.channel, message.channel.server.id)
-	if message.channel.is_private == False and message.server.id == '106293726271246336': #main server commands
-		await bbd.logmessage()
-		if bbd.slowM() == True:
-			await bbd.startslowmode()
-		elif message.content.startswith('!slowmode'):
-			await bbd.slowmode()
-		""" DISABLED UNTIL MONTHLY KARAOKE STARTS
-		elif message.content.startswith("!raisehand"):
-			await bbd.raisehand()
-		elif message.content.startswith('!klist'):
-			await bbd.klist()
-		elif message.content.startswith('!kskip'):
-			await bbd.kskip()
-		"""
+	await bot.process_commands(message)
 
-	#music bot
-	if message.content.startswith("!yt"):
-		await BASEDBOTmusic.musicbot(client).playmusic(message, message.server.id)
-
-	#dn commands
-	dn = BASEDBOTdn.dragonnest(client, message)
-	if message.content.lower().startswith('!pug') and message.channel.id != '106293726271246336' and message.channel.server.id == '106293726271246336':
-		await dn.onoffrole('pug')
-	elif message.content.lower().startswith('!trade') and message.channel.id != '106293726271246336':
-		await dn.onoffrole('trade')
-	elif message.content.lower().startswith('!pvp') and message.channel.id != '106293726271246336':
-		await dn.onoffrole('pvp')
-	elif message.content.startswith('!viewer') and message.server.id == '106293726271246336':
-		await dn.onoffrole('viewer')
-	elif '@pug' in message.clean_content and message.channel.id != '106300530548039680' and message.channel.server.id == '106293726271246336':
-		await dn.roleMention('pug')
-	elif '@trade' in message.clean_content and message.channel.id != '106301265817931776' and message.channel.server.id == '106293726271246336':
-		await dn.roleMention('trade')
-	elif '@pvp' in message.clean_content and message.channel.id != '106300621459628032' and message.channel.server.id == '106293726271246336':
-		await dn.roleMention('pvp')
-	elif '@viewer' in message.clean_content and message.channel.server.id == '106293726271246336':
-		await dn.roleMention('viewer')
-	elif message.content.lower().startswith('!skillbuilds') or message.content.lower().startswith('!krskillbuilds'):
-		await client.send_message(message.channel, dn.skillbuilds())
-	elif message.content.startswith(('!savednbuild','!savebuild')):
-		await client.send_message(message.channel, dn.savednbuild())
-	elif message.content.startswith(('!editdnbuild','!editbuild')):
-		await client.send_message(message.channel, dn.editdnbuild())
-	elif message.content.startswith(('!deletednbuild','!deletebuild')):
-		await client.send_message(message.channel, dn.deletednbuild())
-	elif message.content.startswith(('!mydnbuilds','!mybuilds')):
-		await dn.mydnbuilds()
-	elif message.content.startswith('$') and len(message.content.split()) == 1:
-		await dn.customdncommands()
-	elif message.content.lower().startswith('!enhance') and message.channel.id != '106293726271246336':
-		await dn.enhancement()
-	elif message.content.lower().startswith('!sa'):
-		await dn.SA()
-	elif message.channel.id == '107718615452618752': # skill-builds channel auto skill build distributor
-		await dn.autobuilds()
-
-	#overwatch commands
-	ow = BASEDBOTow.overwatch(client, message)
-	if len(message.content.split()) > 1 and message.content.lower().split()[0] == '!ow' and message.channel.id != '106293726271246336':
-		await client.send_message(message.channel, ow.owcheck(message.content[4:]))
-		return
-	elif message.content.startswith('|trivia') and ow.tCheck(message.server.id) != True and message.channel.id != '106293726271246336':
-		await ow.trivia(message.server.id)
-	elif message.content.startswith('|stop') and message.channel.id != '106293726271246336':
-		await ow.stopTrivia(message.server.id)
-	elif message.content.startswith('|points') and message.channel.id != '106293726271246336':
-		await ow.points(message.server.id)
-
-	#BNS commands
-	bns = BASEDBOTbns.bladeandsoul(client, message)
-	if message.content.startswith('!bnstree'):
-		await client.send_message(message.channel, bns.bnstree())
-	elif message.content.startswith('!bnsmarket'):
-		await bns.bnsmarket()
-		return
-	elif message.content.lower().startswith('!bns') and message.channel.id != '106293726271246336 88422130479276032 124934505810100224 146298657765851137 144803652635328512':
-		await bns.bnssearch()
-	elif message.content.startswith('!savebnsbuild'):
-		await client.send_message(message.channel, bns.savebnsbuild())
-	elif message.content.startswith('!editbnsbuild'):
-		await client.send_message(message.channel, bns.editbnsbuild())
-	elif message.content.startswith('!deletebnsbuild'):
-		await client.send_message(message.channel, bns.deletebnsbuild())
-	elif message.content.startswith('!mybnsbuilds'):
-		await bns.mybnsbuilds()
-	elif message.content.lower().startswith('!mspguide'):
-		await client.send_message(message.channel, 'https://drive.google.com/file/d/0Bx5A-bjrg1p1aVlJZElJV3JoWk0/view')
-	elif message.content.startswith('!!'):
-		await bns.prefixbnscommands()
-
-	if message.content.startswith('!donkayme'):#these are random ass test commands
-		await client.add_reaction(message, "🇩")
-		await client.add_reaction(message, "🇴")
-		await client.add_reaction(message, "🇳")
-		await client.add_reaction(message, "🇰")
-		await client.add_reaction(message, "🇦")
-		await client.add_reaction(message, "🇾")
-	if message.content.startswith('!boop'):
-		await client.add_reaction(message, "🅱")
-		await client.add_reaction(message, "🅾")
-		await client.add_reaction(message, "🇴")
-		await client.add_reaction(message, "🅿")
-	if message.content.startswith('!bruh') and message.channel.id != '106293726271246336':
-		s = '🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿 😂 👌'
-		n = s.split()
-		for i in range(15):
-			r = random.randint(0, len(n)-1)
-			await client.add_reaction(message, n[r])
-
-	#games
-	g = BASEDBOTgames.games(client, message)
-	if message.content in BASEDBOTetc.unicodeResponses:
-		await client.send_message(message.channel, BASEDBOTetc.unicodeResponses[message.content.lower().split()[0]])
-	elif message.content.startswith('!c4'):
-		await g.c4()
-	elif message.content.startswith('!duel') and str(message.channel.id) not in '91518345953689600 106293726271246336':    
-		await g.duel()
-	elif message.content.lower().startswith('!rps'):
-		await g.rps()
-	elif message.content.startswith('!shoot') and len(message.content.split()) == 2:
-		await g.shoots()
-	elif message.content.startswith('!trivia') and message.channel.id != '106293726271246336':
-		await g.dntrivia()
-
-	#other commands
-	etc = BASEDBOTetc.botetc(client, message)
-	if message.content.startswith('!'):
-		await etc.mainprefixcommands()
-	if '!' in message.content and message.channel.is_private == False and message.server.id not in '119222314964353025':
-		await etc.zealemotes()
-	if message.content.startswith('<@175433427175211008>'):
-		await etc.bbresponse()
-	elif message.content.startswith('!define') and message.channel.id != '106293726271246336':
-		await client.send_message(message.channel, etc.defines())
-	elif message.content.lower().startswith('!mal') and message.channel.id not in '106293726271246336':
-		await etc.mal()
-	elif message.content.startswith('!checktwitch'):
-		await client.send_message(message.channel, etc.checktwitch())
-	elif message.content.startswith('!spookme'):
-		await etc.spookme()
-	elif message.content.startswith('!delete ian'):
-		await etc.deleteian()
-	elif message.content.startswith('!lightproc'):
-		await etc.lightproc()
-	elif message.content.startswith('!yesh') and message.server.id == '90944254297268224':
-		await client.send_message(message.channel, """<:Heck1:235258589621649408><:Heck2:235258604448382978><:Fucking:235256098427240451>\n<:Heck4:235258621955407872><:Man:235256139514773504><:Im:235256149455273984>\n<:Fuckin:235256165804539906><:Cumming:235256179045957633><:Cx:235256191154913280>""")
-	elif message.content.startswith('!gimmepoutine'):
-		await client.send_file(message.channel, 'poutine.jpg')
-	elif (message.content.lower().startswith('!colors') or message.content.lower().startswith('!colorlist')) and message.channel.is_private == False and message.server.id == '106293726271246336':
-		await etc.colors()
-	elif message.content.lower().startswith('!color') and message.channel.is_private == False and message.server.id == '106293726271246336':
-		await etc.color()
-	if message.content.startswith('!msg') and message.author.id == '90886475373109248':
-		await client.send_message(client.get_server(message.content.split()[1]).default_channel, message.content.replace(message.content.split()[0], '').replace(message.content.split()[1], ''))
-	if message.content.startswith('!reload') and message.author.id == '90886475373109248':
-		try:
-			importlib.reload(sys.modules[message.content.split()[1]])
-			await client.send_message(message.channel, 'module {} reloaded'.format(message.content.split()[1]))
-		except Exception as e:
-			await client.send_message(message.channel, 'reloading failed, error is:```{}```'.format(e))
-
-@client.async_event
+@bot.async_event
 def on_ready():
 	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
+	print(bot.user.name)
+	print(bot.user.id)
 	print('------')
-	yield from client.change_presence(game=discord.Game(name='you like a fiddle'))
-	BASEDBOTdiscord.upT = datetime.datetime.now()
+	yield from bot.change_presence(game=discord.Game(name='you like a fiddle'))
+	if not hasattr(bot, 'uptime'):
+		bot.uptime = datetime.datetime.now()
 
-async def main_task():
-	await client.login(dLogin['username'])
-	await client.connect()
+for extension in startup_ext:
+	try:
+		bot.load_extension(extension)
+	except Exception as e:
+		print('Failed to load extension {}\n{}'.format(extension, e))
 
-loop = asyncio.get_event_loop()
-try:
-	loop.run_until_complete(main_task())
-except Exception:
-	loop.run_until_complete(client.close())
-finally:
-	loop.close()
+bot.run(dLogin['username'])
