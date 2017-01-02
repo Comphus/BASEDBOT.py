30c23c98021f022ea2624b5d6d44c6cf4106f923
Gabriel Lopez <gabiota3000@yahoo.com>
2017-01-01 20:07:55 -0500

    Update BASEDBOT.py

--- BASEDBOT.py
+++ BASEDBOT.py
@@ -1,28 +1,27 @@
-from discord.ext import commands
 import discord
+from cogs.utils import checks
+from discord.ext import commands
 import asyncio
 import json
 import datetime
 import random
-import importlib
+import traceback
 import sys
+
 bot = commands.Bot(command_prefix='!',description="I am a bot created by Comphus mainly for the Dragon Nest Community Discord server, but I have many other functions!")
 
 startup_ext = [
-	"botmodules.BASEDBOTgames",
-	"botmodules.BASEDBOTbns",
-	"botmodules.BASEDBOTow",
-	"botmodules.BASEDBOTdn",
-	"botmodules.BASEDBOTetc",
-	"botmodules.BASEDBOTmusic",
-	"botmodules.BASEDBOTdiscord"
+	"cogs.BASEDBOTgames",
+	"cogs.BASEDBOTbns",
+	"cogs.BASEDBOTow",
+	"cogs.BASEDBOTdn",
+	"cogs.BASEDBOTetc",
+	"cogs.BASEDBOTmusic",
+	"cogs.BASEDBOTdiscord"
 ]
 
 with open("C:/discordlogin.json") as j:
 	dLogin = json.load(j)
-
-def is_owner(ctx):
-	return ctx.message.author.id == '90886475373109248'
 
 @bot.event
 async def on_command_error(error, ctx):
@@ -30,24 +29,25 @@ async def on_command_error(error, ctx):
 		await bot.send_message(ctx.message.author, 'This command cannot be used in private messages.')
 	elif isinstance(error, commands.DisabledCommand):
 		await bot.send_message(ctx.message.author, 'Sorry. This command is disabled and cannot be used.')
-	#else:
-	#	print(error)
+	elif isinstance(error, commands.CommandInvokeError):
+		print('In {0.command.qualified_name}:'.format(ctx), file=sys.stderr)
+		traceback.print_tb(error.original.__traceback__)
+		print('{0.__class__.__name__}: {0}'.format(error.original), file=sys.stderr)
 
-@bot.command()
-@commands.check(is_owner)
+@bot.command(hidden=True)
+@checks.is_owner()
 async def reload(extension_name : str):
 	try:
 		bot.unload_extension(extension_name)
 		bot.load_extension(extension_name)
 		await bot.say('module {} reloaded'.format(extension_name))
 	except Exception as e:
-		await bot.say('reloading failed, error is:```\n{}```'.format(e))
+		await bot.say('reloading failed, error is:```\n{}: {}```'.format(type(e).__name__, e))
 
 @bot.event
 async def on_message(message):
 	if bot.user == message.author or message.channel.id == '168949939118800896' or message.author.id in '128044950024617984':
 		return
-
 	await bot.process_commands(message)
 
 @bot.async_event
